0f0ff63beba2892854bf7620c879a9e44762cf8e
nursen <200103098@stu.sdu.edu.kz>
2022-11-27 01:29:12 +0600

    Added ViewModel for MealActivity and YT link clikc listener

--- app/src/main/java/com/example/beam/activities/MealActivity.kt
+++ app/src/main/java/com/example/beam/activities/MealActivity.kt
@@ -1,19 +1,29 @@
 package com.example.beam.activities
 
 import android.annotation.SuppressLint
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.bumptech.glide.Glide
 import com.example.beam.R
 import com.example.beam.databinding.ActivityMainBinding
 import com.example.beam.databinding.ActivityMealBinding
 import com.example.beam.fragments.HomeFragment
+import com.example.beam.pojo.Meal
+import com.example.beam.viewModel.MealViewModel
 
 class MealActivity : AppCompatActivity() {
     private lateinit var binding:ActivityMealBinding
     private lateinit var mealId:String
     private lateinit var mealName:String
     private lateinit var mealThumb:String
+    private lateinit var youtubeLink: String
+
+    private lateinit var mealMVVM: MealViewModel
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,8 +31,40 @@ class MealActivity : AppCompatActivity() {
         binding = ActivityMealBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        mealMVVM = ViewModelProviders.of(this)[MealViewModel::class.java]
+
         getMealInfoFromIntent()
+
         setInfoViews()
+
+        loadingCase()
+
+        mealMVVM.getMealDetail(mealId)
+        observerMealDetailsLiveData()
+
+        onYoutubeImageClick()
+    }
+
+    private fun onYoutubeImageClick() {
+        binding.imgYoutube.setOnClickListener {
+            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(youtubeLink))
+            startActivity(intent)
+        }
+    }
+
+    private fun observerMealDetailsLiveData() {
+        mealMVVM.observerMealDetailsLiveData().observe(this, object : Observer<Meal> {
+            override fun onChanged(t: Meal?) {
+                onResponseCase()
+                val meal = t
+
+                binding.textviewCategory.text = "Category: ${meal!!.strCategory}"
+                binding.textviewCountry.text = "Area: ${meal!!.strArea}"
+                binding.textviewInstructionText.text = meal.strInstructions
+
+                youtubeLink = meal.strYoutube
+            }
+        })
     }
 
     private fun setInfoViews() { //передача изображении
@@ -41,4 +83,24 @@ class MealActivity : AppCompatActivity() {
         mealName = intent.getStringExtra(HomeFragment.MEAL_NAME)!!
         mealThumb = intent.getStringExtra(HomeFragment.MEAL_THUMB)!!
     }
+
+    private fun loadingCase() {
+        binding.progressBar.visibility = View.VISIBLE
+        binding.btnAddToFav.visibility = View.INVISIBLE
+        binding.textviewInstruction.visibility = View.INVISIBLE
+        binding.textviewCategory.visibility = View.INVISIBLE
+        binding.textviewCountry.visibility = View.INVISIBLE
+        binding.imgYoutube.visibility = View.INVISIBLE
+    }
+
+    private fun onResponseCase() {
+        binding.progressBar.visibility = View.INVISIBLE
+        binding.btnAddToFav.visibility = View.VISIBLE
+        binding.textviewInstruction.visibility = View.VISIBLE
+        binding.textviewCategory.visibility = View.VISIBLE
+        binding.textviewCountry.visibility = View.VISIBLE
+        binding.imgYoutube.visibility = View.VISIBLE
+
+    }
+
 }
--- app/src/main/java/com/example/beam/retrofit/MealApi.kt
+++ app/src/main/java/com/example/beam/retrofit/MealApi.kt
@@ -3,10 +3,13 @@ package com.example.beam.retrofit
 import com.example.beam.pojo.MealList
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Query
 
 interface MealApi {
 
     @GET("random.php")
     fun getRandomMeal(): Call<MealList>
 
+    @GET("lookup.php")
+    fun getMealDetails(@Query("i") id: String): Call<MealList>
 }
