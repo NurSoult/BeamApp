5f372bbb30467a1dcce4e5ceab06c7218ab21454
Kadyr Zhassulan <91288110+ZhassulanSDU@users.noreply.github.com>
2022-11-30 03:05:33 +0600

    added dependency ViewModelScope, some code to Mealv

--- app/src/main/java/com/example/beam/viewModel/MealViewModel.kt
+++ app/src/main/java/com/example/beam/viewModel/MealViewModel.kt
@@ -4,14 +4,19 @@ import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.beam.db.MealDatabase
 import com.example.beam.pojo.Meal
 import com.example.beam.pojo.MealList
 import com.example.beam.retrofit.RetrofitInstance
+import kotlinx.coroutines.launch
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class MealViewModel: ViewModel() {
+class MealViewModel(
+    val mealDatabase: MealDatabase
+): ViewModel() {
     private var mealDetailsLiveData = MutableLiveData<Meal>()
 
     fun getMealDetail(id: String) {
@@ -34,5 +39,16 @@ class MealViewModel: ViewModel() {
         return mealDetailsLiveData
     }
 
+    fun insertMeal(meal: Meal){
+        viewModelScope.launch {
+            mealDatabase.mealDao().insertMeal(meal)
+        }
+    }
+
+    fun deleteMeal(meal: Meal){
+        viewModelScope.launch {
+            mealDatabase.mealDao().delete(meal)
+        }
+    }
 
 }
